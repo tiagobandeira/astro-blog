c657f14dee1778ba01294e77f733ed03eccab92c
tiagobandeira <tiagobandeirabarros@gmail.com>
2023-08-03 06:36:59 -0300

    feat: add type checking and create better query
    Make the code easier to maintain in addition to improving the query of
    posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,15 @@
-// import {z, defineCollection } from 'astro:content';
+import {z, defineCollection } from 'astro:content';
 
-// const postCollection = defineCollection({
-//     type: 'content',
-//     schema: z.object({
-//         title: z.string(),
-//         description: z.string(),
-//         date: z.string()
-//     })
-// })
+const postCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.string(),
+        tags: z.array(z.string())
+    })
+})
 
-// export const collections = {
-//     'posts': postCollection
-// }
+export const collections = {
+    'posts': postCollection
+}
